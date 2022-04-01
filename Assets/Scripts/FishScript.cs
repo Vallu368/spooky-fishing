101368f920084656c6f137c911271f1f83f8f75f
vallu368 <vsavolainen22@gmail.com>
2022-04-01 13:42:43 +0300

    kala juttuja ja kalastus animaatio

--- Assets/Scripts/FishScript.cs
+++ Assets/Scripts/FishScript.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 public class FishScript : MonoBehaviour
 {
     public GameObject[] fishes;
+    public int i = 0;
+    public Fishing fishing;
 
+
+    private void Update()
+    {
+        if (fishing.caughtFish)
+        {
+            
+            gameObject.SetActive(true);
+        }
+        else gameObject.SetActive(false);
+
+    }
     public void SpawnRandomPrefab()
     {
-        int index = Random.Range(0, fishes.Length);  //valitsee randomi
-        Instantiate(fishes[index], this.transform);
+        int index = Random.Range(0, fishes.Length);  //valitsee randomi kalan
+        var fish = Instantiate(fishes[index], this.transform); // spawnaa kalan
+
+        Destroy(fish, 2);
+
+
+        
     }
+
+    public void SetGameObjectActive()
+    {
+        gameObject.SetActive(true);
+    }
+
 }
