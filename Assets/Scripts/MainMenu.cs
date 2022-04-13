a5b0624c0980c407c888911dd75923eb281e6760
juujoojaa <thiscantbewhy@gmail.com>
2022-04-13 13:36:34 +0300

    mmm

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Main Scene");
     }
 
     public Dropdown resolutionDropdown;
